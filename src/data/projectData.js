164210d639fc260ab9925410e406f298d209fadb
jsnam <skawotkd12@naver.com>
2025-07-07 11:53:07 +0900

    데이터 수정 1

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,45 +1,77 @@
-import clim_main from '../assets/img/clim_main.png'
-import port_profile from '../assets/img/port_profile.png'
-import dev_code from '../assets/img/dev_code.png'
-import chat from '../assets/img/chat_main.png'
+import clim_main from "../assets/img/clim_main.png";
+import dev_code from "../assets/img/dev_code.png";
+import chat from "../assets/img/chat_main.png";
+import port_profile from "../assets/img/port_profile.png";
 
 const projectData = [
   {
     pr_id: "1",
     pr_team: "T",
-    pr_image: clim_main,
-    pr_title: "정상에서 만나요",
-    pr_detail: "클라이밍을 즐겨하는 사람들을 위해 제작하게 되었습니다.",
-    pr_sitelink: "",
-    pr_date: "2024.07 ~ 2024.09",
+    pr_image: port_profile,
+    pr_title: "Delulu",
+    pr_detail: "웹소설 주인공이나 연예인과 AI 채팅이 가능한 플랫폼입니다.",
+    pr_sitelink: "https://jsnam-portfolio.vercel.app/",
+    pr_date: "2025.06 ~ 2025.07",
+    pr_language: ["typescript", "nextjs", "tailwind"],
   },
   {
     pr_id: "2",
     pr_team: "S",
-    pr_image: dev_code,
-    pr_title: "JSDEV",
-    pr_detail: "프로젝트를 진행할 때 자주 사용하는 기능들을 정리해 놓은 디벨롭 사이트 입니다.",
-    pr_sitelink: "https://jaesang98.github.io/jsDevelop/",
-    pr_date: "2024.09 ~",
+    pr_image: port_profile,
+    pr_title: "JS-Developer",
+    pr_detail:
+      "개발 중 자주 마주치는 이슈나 반복되는 작업들을 정리한 개인 개발 위키입니다",
+    pr_sitelink: "https://jsnam-portfolio.vercel.app/",
+    pr_date: "2025.04 ~ 2025.06",
+    pr_language: ["typescript", "react", "sass"],
   },
   {
     pr_id: "3",
-    pr_team: "S",
+    pr_team: "T",
     pr_image: port_profile,
-    pr_title: "포트폴리오",
-    pr_detail: "제 소개를 위해 만든 포트폴리오 사이트 입니다.",
+    pr_title: "Musing",
+    pr_detail:
+      "서로 좋아하는 음악을 공유하고 다양한 음악 정보를 공유하는 사이트 입니다.",
     pr_sitelink: "https://jsnam-portfolio.vercel.app/",
-    pr_date: "2024.10 ~ 2024.11",
+    pr_date: "2024.12 ~ 2025.04",
+    pr_language: ["typescript", "react", "emotion"],
   },
   {
     pr_id: "4",
-    pr_team: "T",
-    pr_image: chat,
-    pr_title: "HJ-chatBot",
-    pr_detail: "회사사내에서 사용하기 위해 만든 chat AI입니다.",
-    pr_sitelink: "https://hj-chatbot.vercel.app/hjBot/",
-    pr_date: "2024.11 ~ 2024.11",
+    pr_team: "S",
+    pr_image: port_profile,
+    pr_title: "포트폴리오",
+    pr_detail: "소개를 위한 포트폴리오 사이트 입니다.",
+    pr_sitelink: "https://jsnam-portfolio.vercel.app/",
+    pr_date: "2024.10 ~ 2024.11",
   },
+  // {
+  //   pr_id: "1",
+  //   pr_team: "T",
+  //   pr_image: clim_main,
+  //   pr_title: "정상에서 만나요",
+  //   pr_detail: "클라이밍을 즐겨하는 사람들을 위해 제작하게 되었습니다.",
+  //   pr_sitelink: "",
+  //   pr_date: "2024.07 ~ 2024.09",
+  // },
+  // {
+  //   pr_id: "2",
+  //   pr_team: "S",
+  //   pr_image: dev_code,
+  //   pr_title: "JSDEV",
+  //   pr_detail: "프로젝트를 진행할 때 자주 사용하는 기능들을 정리해 놓은 디벨롭 사이트 입니다.",
+  //   pr_sitelink: "https://jaesang98.github.io/jsDevelop/",
+  //   pr_date: "2024.09 ~",
+  // },
+  // {
+  //   pr_id: "4",
+  //   pr_team: "T",
+  //   pr_image: chat,
+  //   pr_title: "HJ-chatBot",
+  //   pr_detail: "회사사내에서 사용하기 위해 만든 chat AI입니다.",
+  //   pr_sitelink: "https://hj-chatbot.vercel.app/hjBot/",
+  //   pr_date: "2024.11 ~ 2024.11",
+  // },
 ];
 
 export default projectData;
--- src/data/projectDetails.js
+++ src/data/projectDetails.js
@@ -25,77 +25,7 @@ import chat4 from "../assets/img/chat4.png";
 
 const projectDetails = [
   {
-    prDetail_id: "1",
-    prDetail_team: "T",
-    prDetail_title: "정상에서 만나요",
-    prDetail_date: "2024.07 ~ 2024.09",
-    prDetail_description:
-      "이 웹사이트는 클라이밍을 즐기는 사람들과 초보자들이 쉽게 접근할 수 있도록 편리한 기능을 제공하는 플랫폼입니다. 이 프로젝트를 통해 리액트 스터디와 실습을 진행하며, 그동안 부족했던 부분을 보충하고 성장하는 것을 목표로 하고 있습니다..",
-    prDetail_techStack: "React, JavaScript, CSS",
-    prDetail_features: [
-      "내 위치를 기반으로 가까운 클라이밍장 확인 및 예약 기능 (네이버 위치 API 사용)",
-      "커뮤니티 및 파티 기능을 통한 소통과 활동 공유",
-      "카카오 및 네이버 로그인을 통한 간편한 사용자 정보 관리",
-    ],
-    prDetail_role:
-      "기획 및 디자인, 화면 퍼블리싱, 프론트엔드 전 페이지 및 공통 기능 개발",
-    prDetail_reflections:
-      "React를 처음 사용하며 기존 Vue와의 상태 관리 및 컴포넌트 구조의 차이를 명확하게 이해하게 되었습니다.",
-    prDetail_images: [
-      clim_main,
-      clim_Login,
-      clim_commu,
-      clim_find,
-      clim_detail,
-      clim_naverMap,
-    ],
-    prDetail_challenges: {
-      problem:
-        "네이버 및 카카오 API 사용 시 백엔드에서 처리하지 않고 프론트에서 직접 호출할 경우, 카카오는 작동하지만 네이버에서 CORS 오류가 발생했습니다.",
-      solution:
-        "백엔드에서 네이버 및 카카오 API 서버와 통신하여 데이터를 전달받았습니다.",
-    },
-    prDetail_outcomes:
-      "프로젝트 초기에는 프론트엔드, 백엔드, 기획자 포함 총 5명이 팀을 구성했으나 개인 사정으로 인해 프론트엔드 1명, 백엔드 1명이 남아 제작하게 되었습니다. 이로 인해 예상보다 시간이 더 소요되었고, 기획된 화면의 수정이 필요했습니다. 추후 고도화 작업을 진행할 예정입니다.",
-    prDetail_links: {
-      projectLink: "",
-      githubLink: "https://github.com/Jaesang98/summit_meeting",
-    },
-  },
-
-  {
-    prDetail_id: "2",
-    prDetail_team: "S",
-    prDetail_title: "JSDEV",
-    prDetail_date: "2024.09 ~",
-    prDetail_description:
-      "프로젝트를 진행할 때 자주 사용하는 기능들을 정리해 놓아 편하게 쓸 수 있도록 제작한 디벨롭 사이트 입니다.",
-    prDetail_techStack: "Vue, JavaScript, CSS",
-    prDetail_features: [
-      "iframe을 사용하여 실제 구현가능",
-      "구현된 iframe화면의 코드를 highlight.js를 사용하여 코드 확인가능",
-    ],
-    prDetail_role:
-      "기획 및 디자인, 화면 퍼블리싱, 프론트엔드 전 페이지 및 공통 기능 개발",
-    prDetail_reflections:
-      "디벨롭 사이트를 통해 자주 사용하는 기능들인 유틸 뿐 아니라 외부 라이브러리 및 API 그리고 각 프레임워크에서 자주 사용하는 기능들을 정리하며 개발 효율성이 크게 향상되었습니다.",
-    prDetail_images: [dev_main, dev_code],
-    prDetail_challenges: {
-      problem:
-        "프로젝트 진행 중 디자인 및 여러 iframe을 클라이언트에서 관리를 하다보니 유지 관리가 어려워지는 문제를 겪었습니다",
-      solution:
-        "디자인 문제를 해결하기 위해 사용자 피드백을 반영하여 UI를 개선하였고, iframe의 대한 url정보를 컴포넌트 기능으로 재구성하여 유지관리를 하였습니다",
-    },
-    prDetail_outcomes:
-      "현재 계속해서 자주 사용하는 기능들을 정리하여 적용하고 있으며 추가로 기능을 코드로 적용하는 것이 아닌 백엔드 기능을 추가해 등록,수정 및 삭제등의 기능을 추가 할 예정입니다.",
-    prDetail_links: {
-      projectLink: "https://jaesang98.github.io/jsDevelop/",
-      githubLink: "https://github.com/Jaesang98/jsDevelop",
-    },
-  },
-
-  {
-    prDetail_id: "3",
+    prDetail_id: "4",
     prDetail_team: "S",
     prDetail_title: "포트폴리오",
     prDetail_date: "2024.10 ~ 2024.11",
@@ -126,37 +56,106 @@ const projectDetails = [
     },
   },
 
-  {
-    prDetail_id: "4",
-    prDetail_team: "T",
-    prDetail_title: "HJ-chatBot",
-    prDetail_date: "2024.11 ~ 2024.11",
-    prDetail_description:
-      "회사 사내에서 서버 팀원과 협력하여 간단한 Chat AI를 구현해보는 것을 목표로 한 학습 프로젝트입니다.",
-    prDetail_techStack: "Vue, JavaScript, SCSS",
-    prDetail_features: [
-      "데스크톱, 모바일, 태블릿에서 반응형으로 동작",
-      "메시지 전송 및 간단한 대화 기능 구현",
-      "사내 내부망에서만 사용 가능",
-    ],
-    prDetail_role:
-      "기획 및 디자인, 화면 퍼블리싱, 프론트엔드 전체 개발 및 공통 기능 구현",
-    prDetail_reflections:
-      "서버 팀원과 협업하며 Chat AI의 구조를 이해하게 되었고, 프론트엔드에서 서버 통신 및 데이터 처리 경험을 쌓았습니다. 간단한 기능이었지만, 프로젝트를 통해 Vue의 활용 능력이 한층 향상되었습니다.",
-    prDetail_images: [chat_main, chat, chat2, chat3, chat4],
-    prDetail_challenges: {
-      problem:
-        "서버와의 통신 구조와 간단한 AI 응답 생성 로직 설계에서 어려움을 겪었습니다. 또한, 프론트엔드 배포를 Vercel을 사용했는데, 기본적으로 로컬 환경에서는 정상 작동했으나 Vercel 환경에서 배포 후 내부망을 사용하더라도 일부 기능이 제대로 동작하지 않는 문제가 있었습니다.",
-      solution:
-        "서버 팀원의 도움으로 통신 구조를 설계하고 문제를 점검했으며, Vercel 배포 환경에서 발생하는 네트워크 문제를 디버깅하여 설정을 조정하려고 노력했습니다. 이후 일부 기능은 로컬 환경에서만 안정적으로 동작하도록 설정했습니다.",
-    },
-    prDetail_outcomes:
-      "사내 환경에서 사용할 수 있는 간단한 Chat AI를 구현하며, 프론트엔드와 백엔드 간 협업의 중요성을 느꼈습니다. 또한, Vue와 SCSS를 활용해 반응형 UI를 효율적으로 구성하는 능력을 향상시켰습니다. Vercel 배포 경험을 통해 클라우드 배포의 특징과 내부망에서의 제약 사항을 이해할 수 있었습니다.",
-    prDetail_links: {
-      projectLink: "https://hj-chatbot.vercel.app/hjBot/",
-      githubLink: "https://github.com/Jaesang98/HJ-Chatbot",
-    },
-  },
+  // {
+  //   prDetail_id: "1",
+  //   prDetail_team: "T",
+  //   prDetail_title: "정상에서 만나요",
+  //   prDetail_date: "2024.07 ~ 2024.09",
+  //   prDetail_description:
+  //     "이 웹사이트는 클라이밍을 즐기는 사람들과 초보자들이 쉽게 접근할 수 있도록 편리한 기능을 제공하는 플랫폼입니다. 이 프로젝트를 통해 리액트 스터디와 실습을 진행하며, 그동안 부족했던 부분을 보충하고 성장하는 것을 목표로 하고 있습니다..",
+  //   prDetail_techStack: "React, JavaScript, CSS",
+  //   prDetail_features: [
+  //     "내 위치를 기반으로 가까운 클라이밍장 확인 및 예약 기능 (네이버 위치 API 사용)",
+  //     "커뮤니티 및 파티 기능을 통한 소통과 활동 공유",
+  //     "카카오 및 네이버 로그인을 통한 간편한 사용자 정보 관리",
+  //   ],
+  //   prDetail_role:
+  //     "기획 및 디자인, 화면 퍼블리싱, 프론트엔드 전 페이지 및 공통 기능 개발",
+  //   prDetail_reflections:
+  //     "React를 처음 사용하며 기존 Vue와의 상태 관리 및 컴포넌트 구조의 차이를 명확하게 이해하게 되었습니다.",
+  //   prDetail_images: [
+  //     clim_main,
+  //     clim_Login,
+  //     clim_commu,
+  //     clim_find,
+  //     clim_detail,
+  //     clim_naverMap,
+  //   ],
+  //   prDetail_challenges: {
+  //     problem:
+  //       "네이버 및 카카오 API 사용 시 백엔드에서 처리하지 않고 프론트에서 직접 호출할 경우, 카카오는 작동하지만 네이버에서 CORS 오류가 발생했습니다.",
+  //     solution:
+  //       "백엔드에서 네이버 및 카카오 API 서버와 통신하여 데이터를 전달받았습니다.",
+  //   },
+  //   prDetail_outcomes:
+  //     "프로젝트 초기에는 프론트엔드, 백엔드, 기획자 포함 총 5명이 팀을 구성했으나 개인 사정으로 인해 프론트엔드 1명, 백엔드 1명이 남아 제작하게 되었습니다. 이로 인해 예상보다 시간이 더 소요되었고, 기획된 화면의 수정이 필요했습니다. 추후 고도화 작업을 진행할 예정입니다.",
+  //   prDetail_links: {
+  //     projectLink: "",
+  //     githubLink: "https://github.com/Jaesang98/summit_meeting",
+  //   },
+  // },
+
+  // {
+  //   prDetail_id: "2",
+  //   prDetail_team: "S",
+  //   prDetail_title: "JSDEV",
+  //   prDetail_date: "2024.09 ~",
+  //   prDetail_description:
+  //     "프로젝트를 진행할 때 자주 사용하는 기능들을 정리해 놓아 편하게 쓸 수 있도록 제작한 디벨롭 사이트 입니다.",
+  //   prDetail_techStack: "Vue, JavaScript, CSS",
+  //   prDetail_features: [
+  //     "iframe을 사용하여 실제 구현가능",
+  //     "구현된 iframe화면의 코드를 highlight.js를 사용하여 코드 확인가능",
+  //   ],
+  //   prDetail_role:
+  //     "기획 및 디자인, 화면 퍼블리싱, 프론트엔드 전 페이지 및 공통 기능 개발",
+  //   prDetail_reflections:
+  //     "디벨롭 사이트를 통해 자주 사용하는 기능들인 유틸 뿐 아니라 외부 라이브러리 및 API 그리고 각 프레임워크에서 자주 사용하는 기능들을 정리하며 개발 효율성이 크게 향상되었습니다.",
+  //   prDetail_images: [dev_main, dev_code],
+  //   prDetail_challenges: {
+  //     problem:
+  //       "프로젝트 진행 중 디자인 및 여러 iframe을 클라이언트에서 관리를 하다보니 유지 관리가 어려워지는 문제를 겪었습니다",
+  //     solution:
+  //       "디자인 문제를 해결하기 위해 사용자 피드백을 반영하여 UI를 개선하였고, iframe의 대한 url정보를 컴포넌트 기능으로 재구성하여 유지관리를 하였습니다",
+  //   },
+  //   prDetail_outcomes:
+  //     "현재 계속해서 자주 사용하는 기능들을 정리하여 적용하고 있으며 추가로 기능을 코드로 적용하는 것이 아닌 백엔드 기능을 추가해 등록,수정 및 삭제등의 기능을 추가 할 예정입니다.",
+  //   prDetail_links: {
+  //     projectLink: "https://jaesang98.github.io/jsDevelop/",
+  //     githubLink: "https://github.com/Jaesang98/jsDevelop",
+  //   },
+  // },
+  // {
+  //   prDetail_id: "4",
+  //   prDetail_team: "T",
+  //   prDetail_title: "HJ-chatBot",
+  //   prDetail_date: "2024.11 ~ 2024.11",
+  //   prDetail_description:
+  //     "회사 사내에서 서버 팀원과 협력하여 간단한 Chat AI를 구현해보는 것을 목표로 한 학습 프로젝트입니다.",
+  //   prDetail_techStack: "Vue, JavaScript, SCSS",
+  //   prDetail_features: [
+  //     "데스크톱, 모바일, 태블릿에서 반응형으로 동작",
+  //     "메시지 전송 및 간단한 대화 기능 구현",
+  //     "사내 내부망에서만 사용 가능",
+  //   ],
+  //   prDetail_role:
+  //     "기획 및 디자인, 화면 퍼블리싱, 프론트엔드 전체 개발 및 공통 기능 구현",
+  //   prDetail_reflections:
+  //     "서버 팀원과 협업하며 Chat AI의 구조를 이해하게 되었고, 프론트엔드에서 서버 통신 및 데이터 처리 경험을 쌓았습니다. 간단한 기능이었지만, 프로젝트를 통해 Vue의 활용 능력이 한층 향상되었습니다.",
+  //   prDetail_images: [chat_main, chat, chat2, chat3, chat4],
+  //   prDetail_challenges: {
+  //     problem:
+  //       "서버와의 통신 구조와 간단한 AI 응답 생성 로직 설계에서 어려움을 겪었습니다. 또한, 프론트엔드 배포를 Vercel을 사용했는데, 기본적으로 로컬 환경에서는 정상 작동했으나 Vercel 환경에서 배포 후 내부망을 사용하더라도 일부 기능이 제대로 동작하지 않는 문제가 있었습니다.",
+  //     solution:
+  //       "서버 팀원의 도움으로 통신 구조를 설계하고 문제를 점검했으며, Vercel 배포 환경에서 발생하는 네트워크 문제를 디버깅하여 설정을 조정하려고 노력했습니다. 이후 일부 기능은 로컬 환경에서만 안정적으로 동작하도록 설정했습니다.",
+  //   },
+  //   prDetail_outcomes:
+  //     "사내 환경에서 사용할 수 있는 간단한 Chat AI를 구현하며, 프론트엔드와 백엔드 간 협업의 중요성을 느꼈습니다. 또한, Vue와 SCSS를 활용해 반응형 UI를 효율적으로 구성하는 능력을 향상시켰습니다. Vercel 배포 경험을 통해 클라우드 배포의 특징과 내부망에서의 제약 사항을 이해할 수 있었습니다.",
+  //   prDetail_links: {
+  //     projectLink: "https://hj-chatbot.vercel.app/hjBot/",
+  //     githubLink: "https://github.com/Jaesang98/HJ-Chatbot",
+  //   },
+  // },
 ];
 
 export default projectDetails;
